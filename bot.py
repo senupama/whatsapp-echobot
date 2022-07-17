5efc4b545f6eb97c16876a03d6bd43d2bbe178c9
senupama <107684425+senupama@users.noreply.github.com>
2022-07-17 15:52:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,26 +15,9 @@ def bot():
   
     # creating object of MessagingResponse
     response = MessagingResponse()
-  
-    # User Query
-    q = user_msg + "geeksforgeeks.org"
-  
-    # list to store urls
-    result = []
-  
-    # searching and storing urls
-    for i in search(q, tld='co.in', num=6, stop=6, pause=2):
-        result.append(i)
-  
-    # displaying result
-    msg = response.message(f"--- Result for '{user_msg}' are  ---")
-  
-    msg = response.message(result[0])
-    msg = response.message(result[1])
-    msg = response.message(result[2])
-    msg = response.message(result[3])
-    msg = response.message(result[4])
-  
+    reply=response.message()
+    if user_msg=='hi':
+        
     return str(response)
 
 if __name__ == '__main__':
